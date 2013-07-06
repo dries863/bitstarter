7e0d5144a542a56e21934063e102d746f8845e23
Koen Driessen <koen.driessen@gmail.com>
2013-07-06 15:43:36 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var fs = require('Buffer');
-var fs = require('buffer');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
