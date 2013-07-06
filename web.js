b3a110dd2ba7bc5e5b296a364dc927db5c0ddc0f
Koen Driessen <koen.driessen@gmail.com>
2013-07-06 15:40:57 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World 123456!!!!11');
-  response.send(fs.readFileSync('./index.html'));
+  response.send(buffer.write(fs.readFileSync('./index.html')));
 });
 
 var port = process.env.PORT || 5000;
