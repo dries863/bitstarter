fd979b6242e13e8035da50c6c1769f56ea51cfdf
Koen Driessen <koen.driessen@gmail.com>
2013-07-08 19:48:10 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var fs = require('fs');
-//var buffer = require('Buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 123456!!!!11');
+	//response.send('Hello World 123456!!!!11');
+	buf = new Buffer(256);
+	len = buf.write('\u00bd + \u00bc = \u00be', 0);
+	response.send(len + " bytes: " + buf.toString('utf8', 0, len));
   //response.send(buffer.toString(fs.readFileSync('./index.html')));
 });
 
