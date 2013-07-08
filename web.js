2337de01205e4c78c7701243d36d5492e33d5db7
Koen Driessen <koen.driessen@gmail.com>
2013-07-08 20:06:27 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
 	response.send('Hello World 123456!!!!11');
 	//buf = new Buffer(fs.readFileSync('./index.html'));
 	var buffer = new Buffer(fs.readFileSync('./index.html'), "utf-8")
-	//response.send(buf.toString('utf8', 0, ));
+	response.send(buffer.toString());
 	//response.send(buffer.toString(fs.readFileSync('./index.html')));
 });
 
